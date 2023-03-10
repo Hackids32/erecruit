9ab5f92a22aa49e627fc77046d213c62701a981d
joniskandar32 <94894671+joniskandar32@users.noreply.github.com>
2023-03-10 07:30:45 +0700

    Update Sendemail.php

--- application/controllers/Sendemail.php
+++ application/controllers/Sendemail.php
@@ -25,18 +25,18 @@ class Sendemail extends CI_Controller
 
         // SMTP configuration
         $mail->isSMTP();
-        $mail->Host     = 'ntc.co.id'; //sesuaikan sesuai nama domain hosting/server yang digunakan
+        $mail->Host     = ''; //sesuaikan sesuai nama domain hosting/server yang digunakan
         $mail->SMTPAuth = true;
-        $mail->Username = 'it1@ntc.co.id'; // user email
-        $mail->Password = 'ntc2743.'; // password email
+        $mail->Username = '; // user email
+        $mail->Password = ''; // password email
         $mail->SMTPSecure = 'ssl';
         $mail->Port     = 465;
 
-        $mail->setFrom('hris.noreply@ntc.co.id', 'Admin HRIS'); // user email
-        $mail->addReplyTo('it1@ntc.co.id', 'IT Dept'); //user email
+        $mail->setFrom('xxx@email.com', 'Admin HRIS'); // user email
+        $mail->addReplyTo('xxx@email.com', 'IT Dept'); //user email
 
         // Add a recipient
-        $mail->addAddress('inside.man108@gmail.com'); //email tujuan pengiriman email
+        $mail->addAddress('xxx@email.com'); //email tujuan pengiriman email
 
         // Email subject
         $mail->Subject = '[Verifikasi Akun NTC Online Recruitment]'; //subject email
